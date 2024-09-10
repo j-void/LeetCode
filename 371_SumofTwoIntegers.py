6f76f01095fc3225781f15b9c1992910c2c79e59
Janmesh Ukey <janmeshukey@icloud.com>
2024-09-10 12:26:16 -0600

    Create 371_SumofTwoIntegers.py
    https://leetcode.com/problems/sum-of-two-integers/description/

---
+++ 371_SumofTwoIntegers.py
@@ -0,0 +1,10 @@
+class Solution:
+    def getSum(self, a: int, b: int) -> int:
+        mask = 0xffffffff
+
+        while b != 0:
+            carry = (a & b) & mask
+            a = (a ^ b) & mask
+            b = (carry << 1) & mask
+        
+        return a if a <= 0x7FFFFFFF else ~(a ^ mask)
